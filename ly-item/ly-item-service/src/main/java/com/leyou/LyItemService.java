ae24acc8e7a78e8d496806c65999b85c65e8831b
huangh <Teambition:accounts_5e6eee92523534002091d4ab@mail.teambition.com>
2020-05-23 14:41:12 +0800

    商品分类接口

--- ly-item/ly-item-service/src/main/java/com/leyou/LyItemService.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/LyItemService.java
@@ -3,9 +3,11 @@ package com.leyou;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@MapperScan("com.leyou.item.mapper") // 扫描mapper包
 public class LyItemService {
 
     public static void main(String[] args) {
