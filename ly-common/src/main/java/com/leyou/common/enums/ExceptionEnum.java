ff4d32632e319e33b9306ac2060875db088dfb4b
huangh <Teambition:accounts_5e6eee92523534002091d4ab@mail.teambition.com>
2020-05-26 10:53:02 +0800

    改造上传图片到FastDFS

--- ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
+++ ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
@@ -11,7 +11,9 @@ public enum ExceptionEnum {
 
     BRAND_NOT_FOUND(404, "品牌不存在"),
     CATEGORY_NOT_FOUND(404, "商品分类没查到"),
-    BRAND_SAVE_ERROR(500, "新增品牌失败");
+    BRAND_SAVE_ERROR(500, "新增品牌失败"),
+    INVAILD_FILE_TYPE(500, "无效图片类型"),
+    UPLOAD_FILE_ERROR(500, "上传图片失败");
 
     private int code;
     private String msg;
--- ly-upload/src/main/java/com/leyou/upload/service/UploadService.java
+++ ly-upload/src/main/java/com/leyou/upload/service/UploadService.java
@@ -1,55 +1,62 @@
 package com.leyou.upload.service;
 
-import com.leyou.upload.web.UploadController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
+import com.leyou.common.enums.ExceptionEnum;
+import com.leyou.common.exception.LyException;
+import com.leyou.upload.config.UploadProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.util.Arrays;
-import java.util.List;
 
 @Service
+@Slf4j
+@EnableConfigurationProperties(UploadProperties.class)
 public class UploadService {
 
-    private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
+    @Autowired
+    private FastFileStorageClient storageClient;
+
+    @Autowired
+    private UploadProperties prop;
 
     // 支持的文件类型
-    private static final List<String> suffixes = Arrays.asList("image/png", "image/jpeg");
+    //private static final List<String> suffixes = Arrays.asList("image/png", "image/jpeg");
 
     public String upload(MultipartFile file) {
         try {
             // 1、图片信息校验
             // 1)校验文件类型
             String type = file.getContentType();
-            if (!suffixes.contains(type)) {
-                logger.info("上传失败，文件类型不匹配：{}", type);
-                return null;
+            if (!prop.getAllowsTypes().contains(type)) {
+                log.info("上传失败，文件类型不匹配：{}", type);
+                throw new LyException(ExceptionEnum.INVAILD_FILE_TYPE);
             }
             // 2)校验图片内容
             BufferedImage image = ImageIO.read(file.getInputStream());
             if (image == null) {
-                logger.info("上传失败，文件内容不符合要求");
-                return null;
+                log.info("上传失败，文件内容不符合要求");
+                throw new LyException(ExceptionEnum.INVAILD_FILE_TYPE);
             }
-            // 2、保存图片
-            // 2.1、生成保存目录
-            File dir = new File("D:\\heima\\upload");
-            if (!dir.exists()) {
-                dir.mkdirs();
-            }
-            // 2.2、保存图片
-            file.transferTo(new File(dir, file.getOriginalFilename()));
 
-            // 2.3、拼接图片地址
-            String url = "http://image.leyou.com/upload/" + file.getOriginalFilename();
-
-            return url;
+            // 2、将图片上传到FastDFS
+            // 2.1、获取文件后缀名
+            String extension = StringUtils.substringAfterLast(file.getOriginalFilename(), ".");
+            // 2.2、上传
+            StorePath storePath = this.storageClient.uploadFile(
+                    file.getInputStream(), file.getSize(), extension, null);
+            // 2.3、返回完整路径
+            return prop.getBaseUrl() + storePath.getFullPath();
         } catch (Exception e) {
-            return null;
+            // 上传失败
+            log.error("上传图片失败", e);
+            throw new LyException(ExceptionEnum.UPLOAD_FILE_ERROR);
         }
     }
 }
