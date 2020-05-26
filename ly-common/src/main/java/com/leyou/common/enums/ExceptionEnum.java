81b595ae2829c0863b9ccbdb8822ae16b88f4279
huangh <Teambition:accounts_5e6eee92523534002091d4ab@mail.teambition.com>
2020-05-26 15:59:46 +0800

    规格组列表

--- ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
+++ ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
@@ -11,8 +11,9 @@ public enum ExceptionEnum {
 
     BRAND_NOT_FOUND(404, "品牌不存在"),
     CATEGORY_NOT_FOUND(404, "商品分类没查到"),
+    SPEC_GROUP_NOT_FOUND(404, "商品规格组不存在"),
     BRAND_SAVE_ERROR(500, "新增品牌失败"),
-    INVAILD_FILE_TYPE(500, "无效图片类型"),
+    INVAILD_FILE_TYPE(400, "无效图片类型"),
     UPLOAD_FILE_ERROR(500, "上传图片失败");
 
     private int code;
---
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/service/SpecificationService.java
@@ -0,0 +1,28 @@
+package com.leyou.item.service;
+
+import com.leyou.common.enums.ExceptionEnum;
+import com.leyou.common.exception.LyException;
+import com.leyou.item.mapper.SpecGroupMapper;
+import com.leyou.item.pojo.SpecGroup;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+
+@Service
+public class SpecificationService {
+
+    @Autowired
+    private SpecGroupMapper groupMapper;
+
+    public List<SpecGroup> queryGroupByCid(Long cid) {
+        SpecGroup group = new SpecGroup();
+        group.setCid(cid);
+        List<SpecGroup> list = groupMapper.select(group);
+        if (CollectionUtils.isEmpty(list)) {
+            throw new LyException(ExceptionEnum.SPEC_GROUP_NOT_FOUND);
+        }
+        return list;
+    }
+}
