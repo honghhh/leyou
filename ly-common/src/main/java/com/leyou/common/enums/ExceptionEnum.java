5edabda6865f82d990429e6509559e3c8288d671
huangh <Teambition:accounts_5e6eee92523534002091d4ab@mail.teambition.com>
2020-05-27 14:52:16 +0800

    商品列表接口

--- ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
+++ ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
@@ -15,6 +15,7 @@ public enum ExceptionEnum {
     SPEC_GROUP_SAVE_ERROR(500, "商品规格组新增失败"),
     SPEC_GROUP_DELETE_ERROR(500, "商品规格组删除失败"),
     SPEC_PARAM_NOT_FOUND(404, "商品规格参数不存在"),
+    GOODS_NOT_FOUND(404, "商品不存在"),
     BRAND_SAVE_ERROR(500, "新增品牌失败"),
     INVAILD_FILE_TYPE(400, "无效图片类型"),
     UPLOAD_FILE_ERROR(500, "上传图片失败");
