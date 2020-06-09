d178fd8d85ceb36f9a22f1474f724841e721a9f2
huangh <Teambition:accounts_5e6eee92523534002091d4ab@mail.teambition.com>
2020-06-09 17:46:23 +0800

    用户微服务登录 使用jwt加密并设置cookie成功返回token

--- ly-common/src/main/java/com/leyou/common/utils/JsonUtils.java
+++ ly-common/src/main/java/com/leyou/common/utils/JsonUtils.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.lang.Nullable;
 
 import java.io.IOException;
 import java.util.List;
@@ -20,6 +21,7 @@ public class JsonUtils {
 
     private static final Logger logger = LoggerFactory.getLogger(JsonUtils.class);
 
+    @Nullable
     public static String serialize(Object obj) {
         if (obj == null) {
             return null;
@@ -35,6 +37,7 @@ public class JsonUtils {
         }
     }
 
+    @Nullable
     public static <T> T parse(String json, Class<T> tClass) {
         try {
             return mapper.readValue(json, tClass);
@@ -44,6 +47,7 @@ public class JsonUtils {
         }
     }
 
+    @Nullable
     public static <E> List<E> parseList(String json, Class<E> eClass) {
         try {
             return mapper.readValue(json, mapper.getTypeFactory().constructCollectionType(List.class, eClass));
@@ -53,6 +57,7 @@ public class JsonUtils {
         }
     }
 
+    @Nullable
     public static <K, V> Map<K, V> parseMap(String json, Class<K> kClass, Class<V> vClass) {
         try {
             return mapper.readValue(json, mapper.getTypeFactory().constructMapType(Map.class, kClass, vClass));
@@ -62,6 +67,7 @@ public class JsonUtils {
         }
     }
 
+    @Nullable
     public static <T> T nativeRead(String json, TypeReference<T> type) {
         try {
             return mapper.readValue(json, type);
@@ -70,40 +76,4 @@ public class JsonUtils {
             return null;
         }
     }
-
-    /*@Data
-    @AllArgsConstructor
-    @NoArgsConstructor
-    static class User {
-        String name;
-        Integer age;
-    }
-
-    public static void main(String[] args) {
-        User user = new User("jack", 21);
-        // toString
-        String json = serialize(user);
-        System.out.println("json = " + json);
-
-        // 反序列化
-        User user1 = parse(json, User.class);
-        System.out.println("user1=" + user1);
-
-        // toList
-        String json1 = "[20, -10, 5, 15]";
-        List<Integer> list = parseList(json1, Integer.class);
-        System.out.println("list=" + list);
-
-        // toMap
-        String json2 = "{\"name\":\"Jack\", \"age\": \"21\"}";
-        Map<String, String> map = parseMap(json2, String.class, String.class);
-        System.out.println("map=" + map);
-
-        String json3 = "[{\"name\":\"Jack\", \"age\": \"21\"}, {\"name\":\"Jack\", \"age\": \"21\"}]";
-        List<Map<String, String>> maps = nativeRead(json3, new TypeReference<List<Map<String, String>>>() {
-        });
-        for (Map<String, String> map1 : maps) {
-            System.out.println("map1=" + map1);
-        }
-    }*/
 }
